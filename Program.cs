a5eb4aaf7332989722cb014f4074268430fbb5fd
Kislvlad <vlad0kislyak@gmail.com>
2023-04-11 21:29:52 +0300

    task 3 v1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 3, 5, 6 };
+            int target = 5; 
+            int i = 0;
+            while (nums[i] < target)
+            {
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
